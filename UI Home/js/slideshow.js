887251d85b941a77afd72188e89100edf8638265
Aneesh Male <aneesh.male11@gmail.com>
2013-12-01 23:13:40 -0500

    UI Home
    Contains sign in UI and Sign up UI

---
+++ UI Home/js/slideshow.js
@@ -0,0 +1,12 @@
+$(function() {
+$('.fadein img:gt(0)').hide();
+
+setInterval(function () {
+    $('.fadein :first-child').fadeOut()
+                             .next('img')
+                             .fadeIn()
+                             .end()
+                             .appendTo('.fadein');
+}, 4000); // 4 seconds
+});
+</script>
